4171d570f0603461f381081428decbd3299093e5
Dmytro Chasovskyi <dmitriy.chasovskoy@gmail.com>
2017-08-08 21:10:07 +0300

    Add mobile version of layout

--- static/js/custom.js
+++ static/js/custom.js
@@ -2,18 +2,27 @@
  * Created by Dmytro on 7/18/2017.
  */
 
-$(document).ready( function () {
+$(window).on("load", function () {
+    console.log("onload");
     var page = new Page();
     page.update();
 });
 
 $(window).resize(function () {
+    console.log("resize");
     var page = new Page();
     page.update();
 });
 
 $(window).on("orientationchange", function () {
-    console.log("orientationchange");
+    console.log("onorientationchange");
+    var page = new Page();
+    page.update();
+});
+
+$(document).bind("ajaxSend", function(){
+
+}).bind("ajaxStop", function(){
     var page = new Page();
     page.update();
 });
@@ -21,11 +30,17 @@ $(window).on("orientationchange", function () {
 function Page() {
     var body = new Body();
     var footer = new Footer();
-
+    var inputField = new InputField();
+    var isMobileLayout = $('#mobile').is(':visible');
 
     function updatePage() {
         body.update();
-        footer.update();
+        if (isMobileLayout) {
+            inputField.update();
+        }
+        else {
+            footer.update();
+        }
     }
 
     return {
@@ -36,6 +51,17 @@ function Page() {
 function Body() {
     var FOOTER_HEIGHT = 100, MIN_PAGE_HEIGHT = 600;
     var PAGE_HEIGHT = Math.max(MIN_PAGE_HEIGHT, $(window).height()) - FOOTER_HEIGHT;
+    var isMobileLayout = $('#mobile').is(':visible');
+    calculatePageHeight();
+
+    function calculatePageHeight() {
+        console.log($(window).height(), $(document).height(), MIN_PAGE_HEIGHT);
+        console.log(isMobileLayout);
+        if (isMobileLayout) {
+            PAGE_HEIGHT = $(document).height();
+            FOOTER_HEIGHT = 0;
+        }
+    }
 
     function updateBodyHeight() {
         $('.container').css('height', PAGE_HEIGHT);
@@ -47,8 +73,11 @@ function Body() {
     }
 
     function updateHeight() {
+        console.log(PAGE_HEIGHT, FOOTER_HEIGHT);
         updateBodyHeight();
-        updateFooterHeight();
+        if (!isMobileLayout) {
+            updateFooterHeight();
+        }
     }
 
     return {
@@ -58,8 +87,8 @@ function Body() {
 
 function Footer() {
     function updateLogoImageHeight() {
-    $('#logo-block').css({'padding-bottom': $('#logo').height()});
-}
+        $('#logo-block').css({'padding-bottom': $('#logo').height()});
+    }
 
     function updateFooterTextPaddingTop() {
         var LOGO_HEIGHT = $('#logo').height()/2 - $('#logo-text').height()/2;
@@ -76,73 +105,161 @@ function Footer() {
     };
 }
 
+function InputField() {
+    var centerColumnWidth = $('#mobile').find('.center-column').width()/2,
+        inputs = $('.input-mobile');
 
-$(function () {
-    $('#btn').click(function () {
-            var stock = $('#stock_name').val(),
-                start_date = $('#start_date').val(),
-                end_date = $('#end_date').val(),
-                short_ma = $('#short_ma').val(),
-                long_ma = $('#long_ma').val(),
-                range = $('#range').val();
+    function updateInputFieldWidth() {
+        inputs.css({'width': centerColumnWidth});
+    }
 
-            console.log("Before ajax request");
+    return {
+        update: updateInputFieldWidth
+    };
+}
 
-            $.ajax({
-                url: '/calculate',
-                type: 'GET',
-                data: {
-                    stock: stock,
-                    start_date: start_date,
-                    end_date: end_date,
-                    short_ma: short_ma,
-                    long_ma: long_ma,
-                    range: range
-                },
-                success: function (response) {
-                    var response_object = JSON.parse(response);
-                    var message = response_object["message"];
-                    var messages = message.split("\n");
 
-                    eraseTitle();
-                    eraseMessages();
-
-                    var title = messages[0];
-                    drawTitle(title);
-                    drawMessages(messages);
-                },
-                error: function (response) {
-                    eraseTitle();
-                    eraseMessages();
-                    console.error(response);
-                }
-            });
-        }
-    );
+$('.btn').on('click touchstart', function() {
+    var stockCalculationsDrawer = new StockCalculationsDrawer();
+    stockCalculationsDrawer.getAndDrawCalculations();
 });
 
 
-function eraseTitle() {
-    $('#stock-title').empty()
+function StockCalculationsDrawer() {
+    var stockIdName = undefined,
+        startDateIdName = undefined,
+        endDateIdName = undefined,
+        shortMaIdName = undefined,
+        longMaIdName = undefined,
+        rangeIdName = undefined;
+
+    function strategy() {
+        var isMobileLayout = $('#mobile').is(':visible');
+
+        if (isMobileLayout) {
+            stockIdName = '#stock-name-mobile';
+            startDateIdName = '#start-date-mobile';
+            endDateIdName = '#end-date-mobile';
+            shortMaIdName = '#short-ma-mobile';
+            longMaIdName = '#long-ma-mobile';
+            rangeIdName = '#range';
+        }
+        else {
+            stockIdName = '#stock-name';
+            startDateIdName = '#start-date';
+            endDateIdName = '#end-date';
+            shortMaIdName = '#short-ma';
+            longMaIdName = '#long-ma';
+            rangeIdName = '#range';
+        }
+    }
+
+    function getAndDrawCalculations() {
+        strategy();
+
+        var stock = $(stockIdName).val(),
+            start_date = $(startDateIdName).val(),
+            end_date = $(endDateIdName).val(),
+            short_ma = $(shortMaIdName).val(),
+            long_ma = $(longMaIdName).val(),
+            range = $(rangeIdName).val();
+
+        console.log("Before ajax request");
+
+        $.ajax({
+            url: '/calculate',
+            type: 'GET',
+            data: {
+                stock: stock,
+                start_date: start_date,
+                end_date: end_date,
+                short_ma: short_ma,
+                long_ma: long_ma,
+                range: range
+            },
+            success: function (response) {
+                console.log("response");
+                var response_object = JSON.parse(response);
+                var message = response_object["message"];
+                var messages = message.split("\n"),
+                    title = messages[0];
+
+                var outputDrawer = new OutputDrawer({title: title, messages: messages});
+                outputDrawer.draw();
+            },
+            error: function (response) {
+                var outputDrawer = new OutputDrawer({title: "", messages: ""});
+                outputDrawer.draw();
+
+                console.error(response);
+            }
+        });
+    }
+
+    return {
+        getAndDrawCalculations: getAndDrawCalculations
+    };
 }
 
-function eraseMessages() {
-    $('#stock-calculation').empty()
-}
 
-function drawTitle(title) {
-    var div = document.createElement('div');
-    div.className = 'text-field';
-    div.innerText = title;
-    $('#stock-title').append(div);
-}
+function OutputDrawer(parameters) {
+    var title = parameters.title === undefined ? "" : parameters.title,
+        messages = parameters.messages === undefined ? "" : parameters.messages,
+        stockTitleIdName = undefined,
+        stockCalculationsIdName = undefined;
 
-function drawMessages(messages) {
-    for (var index in messages) {
-        if (Number(index) === 0) continue;
+    function strategy() {
+        if ($('#mobile').is(':visible')) {
+            stockTitleIdName = '#stock-title-mobile';
+            stockCalculationsIdName = '#stock-calculation-mobile';
+        }
+        else {
+            stockTitleIdName = '#stock-title';
+            stockCalculationsIdName = '#stock-calculation';
+        }
+    }
+
+    function drawTitle() {
         var div = document.createElement('div');
         div.className = 'text-field';
-        div.innerText = messages[index];
-        $('#stock-calculation').append(div);
+        div.innerText = title;
+        $(stockTitleIdName).append(div);
     }
+
+    function drawMessages() {
+        for (var index in messages) {
+            if (Number(index) === 0) {
+                continue;
+            }
+            var div = document.createElement('div');
+            div.className = 'text-field';
+            div.innerText = messages[index];
+            $(stockCalculationsIdName).append(div);
+        }
+    }
+
+    // TODO: Doesn't this function do too much?
+    function drawContent() {
+        strategy();
+        eraseContent();
+        drawTitle();
+        drawMessages();
+    }
+
+    function eraseTitle() {
+        $(stockTitleIdName).empty()
+    }
+
+    function eraseMessages() {
+        $(stockCalculationsIdName).empty()
+    }
+
+    function eraseContent() {
+        eraseTitle();
+        eraseMessages();
+    }
+
+    return {
+        draw: drawContent
+    };
 }
