6cd411e4be31b15ba73810e1c756fa6483897fd2
Dmytro Chasovskyi <dmitriy.chasovskoy@gmail.com>
2017-09-08 17:40:31 +0300

    Update Base event with validation of input parameters

--- finance/events/base_event.py
+++ finance/events/base_event.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class BaseEvent(object):
     def __init__(self, open_price, close_price, high_price):
         self.open_price = open_price
@@ -6,3 +9,24 @@ class BaseEvent(object):
 
     def get_events_sequence(self):
         pass
+
+    def _validate_input(self):
+        self._validate_open_price()
+        self._validate_close_price()
+        self._validate_open_and_close_prices_shapes()
+
+    def _validate_open_price(self):
+        if type(self.open_price) is not np.ndarray:
+            raise TypeError("Open price is not numpy array")
+
+    def _validate_close_price(self):
+        if type(self.close_price) is not np.ndarray:
+            raise TypeError("Close price is not numpy array")
+
+    def _validate_high_price(self):
+        if type(self.high_price) is not np.ndarray:
+            raise TypeError("High price is not numpy array")
+
+    def _validate_open_and_close_prices_shapes(self):
+        if self.open_price.shape != self.close_price.shape:
+            raise ValueError("Open price and close price arrays are different shapes")
