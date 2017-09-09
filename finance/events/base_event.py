b67112cff65ab5ef29a5caa13070038fbbe8b262
Dmytro Chasovskyi <dmitriy.chasovskoy@gmail.com>
2017-09-09 20:00:40 +0300

    Add abstraction for events

--- finance/events/base_event.py
+++ finance/events/base_event.py
@@ -2,10 +2,11 @@ import numpy as np
 
 
 class BaseEvent(object):
-    def __init__(self, open_price, close_price, high_price):
+    def __init__(self, open_price, close_price, high_price=None, low_price=None):
         self.open_price = open_price
         self.close_price = close_price
         self.high_price = high_price
+        self.low_price = low_price
 
     def get_events_sequence(self):
         pass
@@ -26,6 +27,10 @@ class BaseEvent(object):
     def _validate_high_price(self):
         if type(self.high_price) is not np.ndarray:
             raise TypeError("High price is not numpy array")
+
+    def _validate_low_price(self):
+        if type(self.low_price) is not np.ndarray:
+            raise TypeError("Low price is not numpy array")
 
     def _validate_open_and_close_prices_shapes(self):
         if self.open_price.shape != self.close_price.shape:
---
+++ finance/events/line_event.py
@@ -0,0 +1,25 @@
+from .base_event import BaseEvent
+
+
+class LineEvent(BaseEvent):
+    def __init__(self, price, time_period, price_value='high'):
+        if price_value == 'high':
+            BaseEvent.__init__(self, None, None, price, None)
+        else:
+            BaseEvent.__init__(self, None, None, None, price)
+        self.price_value = price_value
+        self.time_period = time_period
+        self._validate_input()
+
+    def _validate_input(self):
+        if self.price_value == 'high':
+            self._validate_high_price()
+        else:
+            self._validate_low_price()
+        self._validate_time_period()
+
+    def _validate_time_period(self):
+        if not isinstance(self.time_period, (int, long)):
+            raise TypeError("Time period is not int")
+        if self.time_period < 2:
+            raise ValueError("Time period is less than 2 days")
